3c5a425bc97a6781008f865fdaa9713d6633c624
Martin Schäler <martin.schaeler@sbg.ac.at>
2022-04-27 14:11:22 +0200

    p benchmark p in {0,1-2.0} s=50 #p=2 all sel=1/16

--- benchmark/SelectionTests.h
+++ benchmark/SelectionTests.h
@@ -226,7 +226,7 @@ class SelectionTests {
 public:
     //vector<double> p_values = {1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0};
     vector<double> p_values = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0};
-    vector<double> default_selectivities = {1.0/8.0};
+    vector<double> default_selectivities = {1.0/16.0};
     /*vector<double> default_selectivities = {
             1,1.0/2.0
             , 1.0/4.0
