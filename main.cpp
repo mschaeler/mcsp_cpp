847dd14a3472abcd8bae6eebed5ddb51afa1a86a
Martin Schäler <martin.schaeler@sbg.ac.at>
2022-05-31 09:24:10 +0200

    mono column benchmark HOT and Indexed

--- main.cpp
+++ main.cpp
@@ -169,9 +169,9 @@ int main(int argc, char* argv[]) {
     //vector<DatabaseSystem*> all_dbms = {new Elf_Dbms_Lvl_Cutoffs(), new MyRowiseHyper(), new MyMonetDB()};
     //vector<DatabaseSystem*> all_dbms = {new HOTDBMS(), new MyMonetDB_Indexed()};
     //vector<DatabaseSystem*> all_dbms = {new MyMonetDB_Indexed()};
-    //vector<DatabaseSystem*> all_dbms = {new MyMonetDB_Indexed(), new SortedProjectionDBMS()};
+    vector<DatabaseSystem*> all_dbms = {new MyMonetDB_Indexed(), new HOTDBMS()};
     //vector<DatabaseSystem*> all_dbms = {new Elf_Dbms_Lvl_Ranges_External(),new MyMonetDB_Indexed()};
-    vector<DatabaseSystem*> all_dbms = {new Elf_Dbms_Lvl(), new Elf_Dbms_Lvl_Ranges_External(), new SortedProjectionDBMS(), new MyMonetDB_Indexed(), new MyMonetDB(), new MyHyper(), new MyRowiseHyper(), new HOTDBMS()};
+    //vector<DatabaseSystem*> all_dbms = {new Elf_Dbms_Lvl(), new Elf_Dbms_Lvl_Ranges_External(), new SortedProjectionDBMS(), new MyMonetDB_Indexed(), new MyMonetDB(), new MyHyper(), new MyRowiseHyper(), new HOTDBMS()};
     //vector<DatabaseSystem*> all_dbms = {new Elf_Dbms_Lvl(), new Elf_Dbms_Lvl_Ranges_External(),new MyMonetDB_Indexed()};
     //vector<DatabaseSystem*> all_dbms = {new MyRowiseHyper()};
     SelectionTests::run_mono_column_benchmark(all_dbms, scale , 100, true);
